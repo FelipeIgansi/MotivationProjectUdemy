3b310baf9a015e0eed24a35c622d20967e5b73fa
FelipeIgansi <lipeigansi@gmail.com>
2023-03-04 17:29:33 -0300

    Aula criacao da user activity

--- app/src/main/java/com/example/motivation/UserActivity.kt
+++ app/src/main/java/com/example/motivation/UserActivity.kt
@@ -1,24 +1,42 @@
 package com.example.motivation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import com.example.motivation.databinding.ActivityMainBinding
+import android.widget.Toast
+import com.example.motivation.databinding.ActivityUserBinding
 
 class UserActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityUserBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.buttonSave.setOnClickListener(this)
         supportActionBar?.hide()
     }
 
     override fun onClick(v: View) {
-        if (v.id == R.id.button_save){
-            TODO()
+        if (v.id == R.id.button_save) {
+            handleSave()
+        }
+    }
+
+    private fun handleSave() {
+
+
+
+
+        var name = binding.editName.text.toString()
+        if (name != "") {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            Toast.makeText(this, R.string.validation_mandatory_name, Toast.LENGTH_SHORT).show()
         }
     }
 }
