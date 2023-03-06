686db9cc13513745aade96587c9561dd6eee8eb3
Felipe <lipeigansi@gmail.com>
2023-03-06 14:03:39 -0300

    criacao de filtro para a selecao de icones e alterar a cor de acordo com o click

--- app/src/main/java/com/example/motivation/infra/Constants.kt
+++ app/src/main/java/com/example/motivation/infra/Constants.kt
@@ -4,4 +4,10 @@ class Constants private constructor(){
     object KEY {
         const val USER_NAME = "USER_NAME"
     }
+
+    object FILTER{
+        const val INFINITY = 1
+        const val HAPPY_FACE = 2
+        const val SUNNY = 3
+    }
 }
--- app/src/main/java/com/example/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/example/motivation/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.motivation.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.example.motivation.infra.Constants
 import com.example.motivation.R
 import com.example.motivation.infra.SecurityPreferences
@@ -11,6 +12,8 @@ import com.example.motivation.databinding.ActivityMainBinding
 class  MainActivity : AppCompatActivity(), View.OnClickListener
 {
     private  lateinit var binding : ActivityMainBinding
+    private var categoryId = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,8 +22,13 @@ class  MainActivity : AppCompatActivity(), View.OnClickListener
         supportActionBar?.hide()// esconde a barra de navegacao
 
         handleUserName()
+        handleFilter(R.id.image_infinity)
 
         binding.buttonNewPhrase.setOnClickListener(this)
+        //Images
+        binding.imageHappyFace.setOnClickListener(this)
+        binding.imageInfinity.setOnClickListener(this)
+        binding.imageSunny.setOnClickListener(this)
 
     }
 
@@ -31,7 +39,29 @@ class  MainActivity : AppCompatActivity(), View.OnClickListener
 
     override fun onClick(view: View) {
         if (view.id == R.id.button_new_phrase){
+            var s = ""
+        }else if (view.id in listOf(R.id.image_infinity, R.id.image_happy_face, R.id.image_sunny)){
+            handleFilter(view.id)
+        }
+    }
 
+    private fun handleFilter(id: Int) {
+        binding.imageInfinity.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding.imageHappyFace.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        when (id) {
+            R.id.image_infinity -> {
+                binding.imageInfinity.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = Constants.FILTER.INFINITY
+            }
+            R.id.image_happy_face -> {
+                binding.imageHappyFace.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = Constants.FILTER.HAPPY_FACE
+            }
+            R.id.image_sunny -> {
+                binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = Constants.FILTER.SUNNY
+            }
         }
     }
 }
